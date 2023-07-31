e2dd985a2fe9ea4efc07fdc8f942cd8122e05368
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-31 19:51:16 +0700

    style: create new ui

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -20,8 +20,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String screenText = '0';
 
   @override
   Widget build(BuildContext context) {
@@ -29,32 +36,200 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-              child: Container(
-                color: Theme.of(context).primaryColor,
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    '0',
-                    style: Theme.of(context)
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  screenText,
+                  style: Theme.of(context)
                       .textTheme
                       .headline3!
                       .copyWith(color: Colors.white),
-                  ),
                 ),
               ),
+            ),
           ),
           GridView.count(
             padding: const EdgeInsets.all(0),
             shrinkWrap: true,
             crossAxisCount: 4,
             children: <Widget>[
-              CalculatorButton(backgroundColor: Theme.of(context).primaryColorLight, foregroundColor: Theme.of(context).primaryColorDark, text: 'C'),
-              CalculatorButton(backgroundColor: Theme.of(context).primaryColorLight, foregroundColor: Theme.of(context).primaryColorDark, text: '+/-'),
-              CalculatorButton.Icon(
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorLight,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: 'C',
+                onTap: () {
+                  setState(() {
+                    screenText = '0';
+                  });
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorLight,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '+/-',
+                onTap: () {
+                  // TODO
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorLight,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '%',
+                onTap: () {
+                  // TODO
+                },
+              ),
+              CalculatorButton.icon(
                 backgroundColor: Theme.of(context).primaryColorDark,
                 foregroundColor: Theme.of(context).primaryColorLight,
-                icon: Icons.backspace,
                 text: 'Backspace',
+                icon: Icons.backspace,
+                onTap: () {
+                  setState(() {
+                    if (screenText == '0' ||
+                        screenText == '' ||
+                        screenText.length == 1) {
+                      screenText = '0';
+                    } else {
+                      screenText =
+                          screenText.substring(0, screenText.length - 1);
+                    }
+                  });
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '7',
+                onTap: () {
+                  pressNumber(7);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '8',
+                onTap: () {
+                  pressNumber(8);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '9',
+                onTap: () {
+                  pressNumber(9);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorDark,
+                foregroundColor: Theme.of(context).primaryColorLight,
+                text: '/',
+                onTap: () {
+                  // TODO
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '4',
+                onTap: () {
+                  pressNumber(4);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '5',
+                onTap: () {
+                  pressNumber(5);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '6',
+                onTap: () {
+                  pressNumber(6);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorDark,
+                foregroundColor: Theme.of(context).primaryColorLight,
+                text: 'x',
+                onTap: () {
+                  // TODO
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '1',
+                onTap: () {
+                  pressNumber(1);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '2',
+                onTap: () {
+                  pressNumber(2);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '3',
+                onTap: () {
+                  pressNumber(3);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorDark,
+                foregroundColor: Theme.of(context).primaryColorLight,
+                text: '-',
+                onTap: () {
+                  // TODO
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '0',
+                onTap: () {
+                  pressNumber(0);
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Colors.white,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '.',
+                onTap: () {
+                  setState(() {
+                    if (!screenText.contains('.')) {
+                      screenText = '$screenText.';
+                    }
+                  });
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorLight,
+                foregroundColor: Theme.of(context).primaryColorDark,
+                text: '=',
+                onTap: () {
+                  // TODO
+                },
+              ),
+              CalculatorButton(
+                backgroundColor: Theme.of(context).primaryColorDark,
+                foregroundColor: Theme.of(context).primaryColorLight,
+                text: '+',
+                onTap: () {
+                  // TODO
+                },
               ),
             ],
           ),
@@ -62,46 +237,61 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+
+  void pressNumber(int number) {
+    setState(() {
+      if (screenText == '0') {
+        screenText = '$number';
+      } else {
+        screenText = '$screenText$number';
+      }
+    });
+  }
 }
 
 class CalculatorButton extends StatelessWidget {
   final Color backgroundColor;
   final Color foregroundColor;
+  final IconData? icon;
   final String text;
-  IconData? icon;
+  final void Function() onTap;
 
-  CalculatorButton({super.key,
+  const CalculatorButton({
+    Key? key,
     required this.backgroundColor,
     required this.foregroundColor,
-    required this.text
-  });
+    required this.text,
+    required this.onTap, this.icon,
+  }) : super(key: key);
 
-  CalculatorButton.Icon({super.key,
+  const CalculatorButton.icon({
+    Key? key,
     required this.backgroundColor,
     required this.foregroundColor,
-    required this.icon,
-    required this.text
-  });
+    this.icon,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: onTap,
       child: Container(
         color: backgroundColor,
         child: Center(
-            child: icon == null
-                ? Text(
-              text,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline4!
-                  .copyWith(color: foregroundColor),
-            )
-                : Icon(
-              icon,
-              color: foregroundColor,
-            )
+          child: icon == null
+              ? Text(
+            text,
+            style: Theme.of(context)
+                .textTheme
+                .headline4!
+                .copyWith(color: foregroundColor),
+          )
+              : Icon(
+            icon,
+            color: foregroundColor,
+          ),
         ),
       ),
     );
